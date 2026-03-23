93084e719340f1bbc31254e6e423500dc2e6b80b
hagay kohelet <hagay2506@gmail.com>
2026-03-23 11:30:36 +0200

    only files and folders

---
+++ client/src/components/SelectFood.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/SelectFood.css'
+function SelectFood() {
+  return (
+    <div className='select-food'>
+      <h1>בחר את הארוחה שלך</h1>
+    </div>
+  )
+}
+
+export default SelectFood
