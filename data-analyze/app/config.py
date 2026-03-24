d4c9a15bfcaff7d9ff74227fddea5545356af834
Moshe <moshe@example.com>
2026-03-24 13:13:53 +0200

    feat: adding route to configuration

--- data-analyze/app/config.py
+++ data-analyze/app/config.py
@@ -5,5 +5,5 @@ class Configuration():
     def __init__(self):
         db_host = os.getenv('DAL_HOST','db')
         db_port = os.getenv('DAL_PORT','8001')
-
-        self.db_server = f'http://{db_host}:{db_port}'
+        db_route = os.getenv('DAL_ROUTE','candidates')
+        self.db_server = f'http://{db_host}:{db_port}/{db_route}'
