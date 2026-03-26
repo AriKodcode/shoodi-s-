f1c5de2af872ff25612b1d22db1ee4acd625aa9a
Moshe <moshe@example.com>
2026-03-26 10:23:42 +0200

    fix: env in analyze

--- data-analyze/app/config.py
+++ data-analyze/app/config.py
@@ -6,4 +6,4 @@ class Configuration():
         db_host = os.getenv('DAL_HOST','db')
         db_port = os.getenv('DAL_PORT','8001')
         db_route = os.getenv('DAL_ROUTE','candidates')
-        self.db_server = f'http://{db_host}:{db_port}/{db_route}'
+        self.db_server = f'{db_host}/{db_route}'
